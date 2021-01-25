4f6d9845103ec41e7c5637fd1742e4e2badfea71
ominivac <robertokbs@gmail.com>
2021-01-25 15:00:19 -0300

    add rotas

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-app.use((req, res, next) => {
-  res.status(200).send({
-    mensagem: 'ok, deu certo'
-  });
-});
+//add referencia para rota de produtos
+const rotaProdutos = require('./routes/produtos');
+
+app.use('/produtos', rotaProdutos);
 
 module.exports = app;
